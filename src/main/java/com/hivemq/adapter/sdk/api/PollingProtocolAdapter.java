cb62bd25a97c09564b6ecda45537cc774580d374
Daniel Krueger <daniel.krueger@hivemq.com>
2024-05-14 18:30:02 +0200

    sdk extraction > combine two polling protocol adapter interfaces into one

--- src/main/java/com/hivemq/adapter/sdk/api/PollingProtocolAdapter.java
+++ src/main/java/com/hivemq/adapter/sdk/api/PollingProtocolAdapter.java
@@ -1,19 +1,24 @@
 package com.hivemq.adapter.sdk.api;
 
+import com.hivemq.adapter.sdk.api.config.PollingContext;
 import com.hivemq.adapter.sdk.api.data.ProtocolAdapterDataSample;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public interface PollingProtocolAdapter extends ProtocolAdapter {
 
-    @NotNull CompletableFuture<? extends ProtocolAdapterDataSample> poll();
+    @NotNull CompletableFuture<? extends ProtocolAdapterDataSample> poll(@NotNull PollingContext pollingContext);
 
-    default void onSamplerClosed(){
+    @NotNull List<? extends PollingContext> getSubscriptions();
+
+    int getPollingIntervalMillis();
+
+    int getMaxPollingErrorsBeforeRemoval();
+
+    default void onSamplerClosed() {
+
     }
-
-     int getPollingIntervalMillis();
-
-     int getMaxPollingErrorsBeforeRemoval();
 
 }
