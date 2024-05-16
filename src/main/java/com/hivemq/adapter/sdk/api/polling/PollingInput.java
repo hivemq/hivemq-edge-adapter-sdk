b21330690455e0854afd7cba9a8f7151ea74cf2a
Daniel Krueger <daniel.krueger@hivemq.com>
2024-05-16 11:32:11 +0200

    sdk extraction > add Java Doc

--- src/main/java/com/hivemq/adapter/sdk/api/polling/PollingInput.java
+++ src/main/java/com/hivemq/adapter/sdk/api/polling/PollingInput.java
@@ -3,8 +3,17 @@ package com.hivemq.adapter.sdk.api.polling;
 import com.hivemq.adapter.sdk.api.config.PollingContext;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
 
+/**
+ * Input Object for the poll() method of an {@link PollingProtocolAdapter} containing information tu use during the poll.
+ *
+ * @author Daniel Krüger
+ */
+
 public interface PollingInput {
 
+    /**
+     * @return the polling context that contains information on the mqtt processing for this poll.
+     */
     @NotNull PollingContext getPollingContext();
 
 }
--- src/main/java/com/hivemq/adapter/sdk/api/polling/PollingOutput.java
+++ src/main/java/com/hivemq/adapter/sdk/api/polling/PollingOutput.java
@@ -3,14 +3,47 @@ package com.hivemq.adapter.sdk.api.polling;
 import com.hivemq.adapter.sdk.api.data.DataPoint;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
 
+/**
+ * Output parameter provided to the poll method of an {@link PollingProtocolAdapter}.
+ * <p>
+ * It can be used to
+ * <ul>
+ *   <li>Add data points
+ *   <li>Finish the polling
+ *   <li>Failing the polling
+ * </ul>
+ *
+ * @author Daniel Krüger
+ */
 public interface PollingOutput {
 
+    /**
+     * Adds a data point to this sample.
+     *
+     * @param tagName  the name for the tag of this data point
+     * @param tagValue the value of this data point
+     */
     void addDataPoint(final @NotNull String tagName, final @NotNull Object tagValue);
 
+    /**
+     * Adds the given data point to this sample.
+     *
+     * @param dataPoint the data point to add.
+     */
     void addDataPoint(final @NotNull DataPoint dataPoint);
 
+    /**
+     * Signals Edge that all data points are added and the further processing is done.
+     * If no data points were added until this point, no publish will be created.
+     */
     void finish();
 
+    /**
+     * Signals Edge that something went wronf during the polling of the data.
+     * As a result no publish is created from data, that might have been added before calling fail().
+     *
+     * @param t an throwable indicating what went wrong.
+     */
     void fail(@NotNull Throwable t);
 
 }
