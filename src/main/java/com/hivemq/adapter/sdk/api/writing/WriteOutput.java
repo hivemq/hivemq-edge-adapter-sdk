008abd8368193ead18d908042159898c4ce8ad11
yannick <yannick.weber@hivemq.com>
2024-09-13 14:25:04 +0200

    add bidirectional api add configuration impl

---
+++ src/main/java/com/hivemq/adapter/sdk/api/writing/WriteOutput.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023-present HiveMQ GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hivemq.adapter.sdk.api.writing;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public interface WriteOutput {
+
+    /**
+     * Signals Edge that writing is done.
+     */
+    void finish();
+
+    /**
+     * Signals that something went wrong during the writing.
+     *
+     * @param t Throwable indicating what went wrong.
+     * @param errorMessage an optional error message delivering further insights.
+     */
+    void fail(@NotNull Throwable t, @Nullable String errorMessage, boolean retry);
+
+    /**
+     * Signals that something went wrong during the writing.
+     *
+     * @param errorMessage a message indicating what went wrong.
+     */
+    void fail(@NotNull String errorMessage, boolean retry);
+
+}
