33af874c6bee288cb701bb4339ddaead25fa0a61
Florian-Limpoeck <florian.limpoeck@hivemq.com>
2024-09-13 17:57:06 +0200

    opcua > improved datahub usage in WriteTask

--- src/main/java/com/hivemq/adapter/sdk/api/writing/WritingProtocolAdapter.java
+++ src/main/java/com/hivemq/adapter/sdk/api/writing/WritingProtocolAdapter.java
@@ -33,5 +33,5 @@ public interface WritingProtocolAdapter<C extends WriteContext> extends Protocol
     JsonNode getMqttPayloadJsonSchema(@NotNull C writeContext);
 
     @NotNull
-    Class<? extends WritePayload> getMqttPayloadClass(@NotNull C writeContext);
+    Class<? extends WritePayload> getMqttPayloadClass();
 }
