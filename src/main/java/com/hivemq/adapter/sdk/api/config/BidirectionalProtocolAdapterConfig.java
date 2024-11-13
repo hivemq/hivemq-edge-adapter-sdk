a8d6b8f07fcdba8258cef3c61b5628a2f5169049
Daniel Krueger <daniel.krueger@hivemq.com>
2024-11-13 16:25:30 +0100

    field mapping persistence start

--- src/main/java/com/hivemq/adapter/sdk/api/config/BidirectionalProtocolAdapterConfig.java
+++ src/main/java/com/hivemq/adapter/sdk/api/config/BidirectionalProtocolAdapterConfig.java
@@ -15,12 +15,9 @@
  */
 package com.hivemq.adapter.sdk.api.config;
 
-import com.hivemq.adapter.sdk.api.config.mapping.FieldMapping;
 import org.jetbrains.annotations.NotNull;
 
 public interface BidirectionalProtocolAdapterConfig extends ProtocolAdapterConfig {
 
-    @NotNull
-    FieldMapping getFieldMapping();
 
 }
