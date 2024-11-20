788c260f81ab790ce6c731a1cc0e62494a6871ca
Jochen Mader <jochen.mader@hivemq.com>
2024-11-20 15:29:15 +0100

    Renaming

--- src/main/java/com/hivemq/adapter/sdk/api/eventsv2/EventsService.java
+++ src/main/java/com/hivemq/adapter/sdk/api/eventsv2/EventsService.java
@@ -4,7 +4,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public interface EventSendingService {
+public interface EventsService {
     void publish(Event event);
     List<Event> readEvents(final @Nullable Long sinceTimestamp, final @Nullable Integer limit); 
 }
