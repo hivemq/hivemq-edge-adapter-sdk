047e135f04151648f9b746a19fa730051a7c7bbc
Jochen Mader <jochen.mader@hivemq.com>
2024-11-20 15:48:51 +0100

    Readding Jackson annotations

--- src/main/java/com/hivemq/adapter/sdk/api/eventsv2/Event.java
+++ src/main/java/com/hivemq/adapter/sdk/api/eventsv2/Event.java
@@ -1,5 +1,8 @@
 package com.hivemq.adapter.sdk.api.eventsv2;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Date;
@@ -19,30 +22,49 @@ public abstract class Event {
         BRIDGE
     }
 
+    @JsonProperty(value = "type",required = true)
+    @Schema(name = "type", description = "Type of the event")
     private final @NotNull String type;
 
+    @JsonProperty(value = "source", required = true)
+    @Schema(name = "source", description = "Source of the event")
     private final @NotNull Event.Source source;
 
+    @JsonProperty(value = "title", required = true)
+    @Schema(name = "title", description = "Human readable title")
     private final @NotNull String title;
 
+    @JsonProperty(value = "edgeId", required = true)
+    @Schema(name = "edgeId", description = "Globally unique id of the edge broker")
     private final @NotNull String edgeId;
 
+    @JsonProperty(value = "severity", required = true)
+    @Schema(name = "severity", description = "Severity of the event")
     private final @NotNull Event.Severity severity;
 
-    private final @NotNull Date timestamp;
+    @JsonProperty(value = "created", required = true)
+    @Schema(name = "created", description = "Time at which the event occured")
+    private final @NotNull Date created;
 
+    @JsonCreator
     public Event(
+            @JsonProperty(value = "type",required = true)
             final @NotNull String type,
+            @JsonProperty(value = "title",required = true)
             final @NotNull String title,
+            @JsonProperty(value = "edgeId",required = true)
             final @NotNull String edgeId,
+            @JsonProperty(value = "source",required = true)
             final @NotNull Event.Source source,
+            @JsonProperty(value = "severity",required = true)
             final @NotNull Event.Severity severity,
-            final @NotNull Date timestamp) {
+            @JsonProperty(value = "created",required = true)
+            final @NotNull Date created) {
         this.type = type;
         this.title = title;
         this.edgeId = edgeId;
         this.severity = severity;
-        this.timestamp = timestamp;
+        this.created = created;
         this.source = source;
     }
 
@@ -62,8 +84,8 @@ public abstract class Event {
         return severity;
     }
 
-    public @NotNull Date getTimestamp() {
-        return timestamp;
+    public @NotNull Date getCreated() {
+        return created;
     }
 
     public @NotNull Event.Source getSource() {
@@ -73,21 +95,12 @@ public abstract class Event {
     @Override
     public String toString() {
         return "Event{" +
-                "type='" +
-                type +
-                '\'' +
-                ", source=" +
-                source +
-                ", title='" +
-                title +
-                '\'' +
-                ", edgeId='" +
-                edgeId +
-                '\'' +
-                ", severity=" +
-                severity +
-                ", timestamp=" +
-                timestamp +
+                "type='" + type + '\'' +
+                ", source=" + source +
+                ", title='" + title + '\'' +
+                ", edgeId='" + edgeId + '\'' +
+                ", severity=" + severity +
+                ", created=" + created +
                 '}';
     }
 
@@ -96,16 +109,11 @@ public abstract class Event {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         final Event event = (Event) o;
-        return Objects.equals(type, event.type) &&
-                source == event.source &&
-                Objects.equals(title, event.title) &&
-                Objects.equals(edgeId, event.edgeId) &&
-                severity == event.severity &&
-                Objects.equals(timestamp, event.timestamp);
+        return Objects.equals(type, event.type) && source == event.source && Objects.equals(title, event.title) && Objects.equals(edgeId, event.edgeId) && severity == event.severity && Objects.equals(created, event.created);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(type, source, title, edgeId, severity, timestamp);
+        return Objects.hash(type, source, title, edgeId, severity, created);
     }
 }
